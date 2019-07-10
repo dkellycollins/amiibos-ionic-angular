72301cc55db09b5ca822f2a550180b3e7c5394ec
Devin Kelly-Collins <devin.kellycollins@artisantechgroup.com>
2019-07-09 21:05:51 -0500

    Added a menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,30 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  public get version(): string {
+    return environment.version;
+  }
+
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private readonly platform: Platform,
+    private readonly splashScreen: SplashScreen,
+    private readonly statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+  private async initializeApp(): Promise<void> {
+    await this.platform.ready();
+
+    this.statusBar.styleDefault();
+    this.splashScreen.hide();
   }
 }
