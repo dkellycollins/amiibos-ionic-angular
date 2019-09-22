d4b647f1097429248dd8066022239f351145ebc2
Devin Kelly-Collins <dkellycollins@gmail.com>
2019-09-22 09:20:40 -0500

    Updated the model for UserAmiibos.

--- src/app/amiibos/services/user-amiibos.service.ts
+++ src/app/amiibos/services/user-amiibos.service.ts
@@ -2,50 +2,56 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map, skip } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+export interface UserAmiibo {
+  userUid?: string;
+  amiiboSlug: string;
+  isCollected: boolean;
+}
+
+@Injectable()
 export class UserAmiibosService {
 
-  private readonly collectedAmiibos$: BehaviorSubject<Record<string, boolean>>;
+  private static readonly localStorageKey = 'UserAmiibosService.userAmiibos';
+
+  private readonly collectedAmiibos$: BehaviorSubject<Array<UserAmiibo>>;
 
   public constructor() {
-    this.collectedAmiibos$ = new BehaviorSubject<Record<string, boolean>>(this.load());
+    this.collectedAmiibos$ = new BehaviorSubject<Array<UserAmiibo>>(this.load());
     this.collectedAmiibos$.pipe(skip(1)).subscribe(this.save.bind(this));
   }
 
   public getCollectedAmiibos(): Observable<Array<string>> {
     return this.collectedAmiibos$.pipe(
-      map(collectedAmiibos => Object.entries(collectedAmiibos).filter(([_, collected]) => collected).map(([m]) => m)),
+      map(userAmiibos => userAmiibos.filter(userAmiibo => userAmiibo.isCollected).map(userAmiibo => userAmiibo.amiiboSlug))
     );
   }
 
   public toggleAmiibo(slug: string, collected: boolean) {
-    this.collectedAmiibos$.next({
-      ...this.collectedAmiibos$.getValue(),
-      [slug]: collected
-    });
+    this.collectedAmiibos$.next([
+      ...this.collectedAmiibos$.getValue().filter(userAmiibo => userAmiibo.amiiboSlug !== slug),
+      { amiiboSlug: slug, isCollected: collected }
+    ]);
   }
 
-  private save(collectedAmiibos: Record<string, boolean>) {
+  private save(collectedAmiibos: Array<UserAmiibo>) {
     try {
-      localStorage.setItem('UserAmiibosService.collectedAmiibos', JSON.stringify(collectedAmiibos));
+      localStorage.setItem(UserAmiibosService.localStorageKey, JSON.stringify(collectedAmiibos));
     } catch (error) {
       console.error(error);
     }
   }
 
-  private load(): Record<string, boolean> {
+  private load(): Array<UserAmiibo> {
     try {
-      const data = localStorage.getItem('UserAmiibosService.collectedAmiibos');
+      const data = localStorage.getItem(UserAmiibosService.localStorageKey);
 
       if (!data) {
-        return {};
+        return [];
       }
       return JSON.parse(data);
     } catch (error) {
       console.error(error);
-      return {};
+      return [];
     }
   }
 }
