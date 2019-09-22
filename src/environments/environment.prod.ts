5c8f58fc75096d1a2fe81c3dd59cc74022873ff2
Devin Kelly-Collins <dkellycollins@gmail.com>
2019-09-21 21:46:14 -0500

    Updated prod environment settings.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,14 @@ import { version } from '../../package.json';
 
 export const environment = {
   production: true,
-  version
+  version,
+  firebase: {
+    apiKey: 'AIzaSyB5m7Y9MdL1VkIlCZf7u36deIJjvUrmqZg',
+    authDomain: 'amiibos-firebase.firebaseapp.com',
+    databaseURL: 'https://amiibos-firebase.firebaseio.com',
+    projectId: 'amiibos-firebase',
+    storageBucket: '',
+    messagingSenderId: "247074249215",
+    appId: "1:247074249215:web:ce0062ec9c0b7e8edaafca"
+  }
 };
